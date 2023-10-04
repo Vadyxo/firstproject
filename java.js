411394cd02994426cabdc52a30b52d9dcb3e85f0
Vadyxo <146952192+Vadyxo@users.noreply.github.com>
2023-10-04 15:41:19 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const openPopupButton = document.getElementById('openPopup');
+    const closePopupButton = document.getElementById('closePopup');
+    const popup = document.getElementById('popup');
+
+    openPopupButton.addEventListener('click', function() {
+        popup.style.display = 'block';
+    });
+
+    closePopupButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const openPopupButton = document.getElementById('openPopup1');
+    const closePopupButton = document.getElementById('closePopup');
+    const popup = document.getElementById('popup');
+
+    openPopupButton.addEventListener('click', function() {
+        popup.style.display = 'block';
+    });
+
+    closePopupButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const openPopupButton = document.getElementById('openPopup2');
+    const closePopupButton = document.getElementById('closePopup');
+    const popup = document.getElementById('popup');
+
+    openPopupButton.addEventListener('click', function() {
+        popup.style.display = 'block';
+    });
+
+    closePopupButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const openPopupButton = document.getElementById('openPopup3');
+    const closePopupButton = document.getElementById('closePopup');
+    const popup = document.getElementById('popup');
+
+    openPopupButton.addEventListener('click', function() {
+        popup.style.display = 'block';
+    });
+
+    closePopupButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+});
